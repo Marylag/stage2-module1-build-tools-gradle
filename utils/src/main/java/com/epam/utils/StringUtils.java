cd2161b22468e95a9acb877fcdd837103b774c56
mary <laguta113@gmail.com>
2023-12-01 13:04:08 +0600

    Commit with a solution

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,8 +1,9 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
         if (NumberUtils.isParsable(str)) {
             return NumberUtils.createNumber(str).doubleValue() > 0;
         }
